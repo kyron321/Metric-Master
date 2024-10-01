34e3c8e54f93caecccb9f346e8ea76f855428791
Kyron <kyron@Kyrons-MacBook-Pro-2.local>
2024-10-01 16:35:10 +0100

    feat: create test for website data

--- src/__tests__/app/api/graphql/route.test.ts
+++ src/__tests__/app/api/graphql/route.test.ts
@@ -1,4 +1,4 @@
-import fetch from 'node-fetch'; // Install using: npm install node-fetch
+import fetch from 'node-fetch';
 
 describe('GraphQL API handler (integration test)', () => {
   it('should return 200 status code', async () => {
@@ -16,8 +16,39 @@ describe('GraphQL API handler (integration test)', () => {
       }),
     });
 
-    expect(response.status).toBe(200); // Ensure status is 200
+    expect(response.status).toBe(200);
     const data = await response.json();
-    expect(data).toHaveProperty('data'); // Ensure the response contains data
+    expect(data).toHaveProperty('data');
+  });
+});
+
+describe('GraphQL API handler (integration test)', () => {
+  it('should return websites data', async () => {
+    const response = await fetch('http://localhost:3000/api/graphql', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        query: `
+          query {
+            websites {
+              website
+              userId
+              pagespeedInsights {
+                accessibility
+                bestPractices
+                performance
+                seo
+              }
+            }
+          }
+        `,
+      }),
+    });
+
+    expect(response.status).toBe(200);
+    const data = await response.json();
+    expect(data).toHaveProperty('data'); 
   });
 });
